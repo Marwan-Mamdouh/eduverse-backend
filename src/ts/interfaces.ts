2edbb2475939bd62854d1fb2365fe501e6943cba
marwan <marwanabdalmagied@gmail.com>
2025-10-24 01:47:01 +0300

    feature: add two interfaces and extends one

--- src/ts/interfaces.ts
+++ src/ts/interfaces.ts
@@ -1,8 +1,8 @@
-import { ObjectId } from "mongoose";
+import { ObjectId, Document } from "mongoose";
 
 interface ICourse {
   name: string;
-  courseCouver: string;
+  courseCover: string;
   description: string;
   chapters: string[];
   //   category: enum
@@ -16,11 +16,27 @@ interface IRating {
   rate: number;
 }
 
-interface IUser {
-  id: string;
+interface IUser extends Document {
   name: string;
   email: string;
   password: string;
+  role: "admin" | "user";
+  refreshToken?: string;
+  comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+interface TokenPayload {
+  userId: string;
+  email: string;
   role: string;
 }
-export { ICourse, IRating as rating, IUser };
+
+interface authResponse {
+  success: boolean;
+  message?: string;
+  code: number;
+  error?: any;
+  data?: any;
+}
+
+export { ICourse, IRating as rating, IUser, TokenPayload, authResponse };
